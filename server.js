aee32878eab38ad32fc118e1dbbb3e6619f07f53
Richard Casto <richardcasto@Richards-MBP.hsd1.md.comcast.net>
2018-05-08 13:24:15 -0400

    getting some of the starter setup going

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+var port = process.env.port || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+app.listen(port, 
+    () => console.log(`Server started on port ${port}`));
